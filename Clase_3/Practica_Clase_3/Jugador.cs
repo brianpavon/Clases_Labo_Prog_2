154e6db808152cf4b3a124b8e7bd5799fe80eaf9
brianpavon <53911453+brianpavon@users.noreply.github.com>
2020-04-06 22:06:30 -0300

    FINAL CLASE 3

---
+++ Clase_3/Practica_Clase_3/Jugador.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Clase_3
+{
+    
+
+    class Jugador
+    {
+        //para crear una instancia debemos crear un constructor, de todas maneras esta por defecto.
+        //constructores estaticos, son publicos, se corre antes que el constructor de instancia,inicializa las clases,no puede recibir parametros
+        const int DERECHA = 1;
+        const int IZQUIERDA = 2;
+        const int ABAJO = 3;
+        const int ARRIBA = 4;
+
+
+        public string nombre;
+        public static int numeroJugadores;
+        public int vida;
+        //private KeyValuePair<int, int> posicio;//deja poner un tipo de dato con 2 valores
+        private int posicionY;
+        private int posicionX;
+        public int movimiento;
+        public bool isAlive;
+        static Jugador()
+        {
+            numeroJugadores = 0;
+        }
+        
+
+        public Jugador(string nombre, bool posicionDerecha)//:this()//este seria el constructor, con : llamo a un constructor que este privado u otro sin parametros
+        {
+            Jugador.numeroJugadores++;
+            this.nombre = nombre;
+            this.vida = 100;
+            this.isAlive = true;
+            posicionX = 10;
+            posicionY = 10;
+            movimiento = DERECHA;
+            //this.movivimiento = 0;
+            if (posicionDerecha )
+            {
+                posicionX = 70;
+                movimiento = IZQUIERDA;
+            }
+            pintar();
+        }
+
+        private void pintar()
+        {
+            Console.CursorLeft = posicionX;
+            Console.CursorTop = posicionY;
+            Console.Write("█");
+        }
+
+        public void mover()
+        {
+            Console.CursorLeft = posicionX;
+            Console.CursorTop = posicionY;
+            Console.Write(" ");
+
+            switch(movimiento)
+            {
+                case DERECHA:
+                    posicionX++;
+                    break;
+                case IZQUIERDA:
+                    posicionX--;
+                    break;
+                case ARRIBA:
+                    posicionY--;
+                    break;
+                case ABAJO:
+                    posicionY++;
+                    break;
+            }
+            pintar();
+                
+        }
+        
+    }
+}
---
+++ Clase_3/Practica_Clase_3/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Practica_Clase_3
+{
+    class Program
+    {
+        const int DERECHA = 1;
+        const int IZQUIERDA = 2;
+        const int ABAJO = 3;
+        const int ARRIBA = 4;
+        static void Main(string[] args)
+        {
+            //si declaramos una constante que usaremos en todo el programa se declara asi, solo existe en el scoop que se declare
+            //const int LIMITEDERECHA = 80;
+
+            //seguimos con el ejemplo de un jueguito
+            
+            Jugador.numeroJugadores = 0;
+            //Jugador jugador1;
+
+            //jugador1 = new Jugador("Panchito",true);
+            Jugador[] jugadores = new Jugador[4];//declaro un array de jugadores
+
+
+            //Jugador jugador2;
+            //jugador2 = new Jugador("Cachita",true);
+
+            ConsoleKeyInfo consoleKeyInfo;//me guarda lo que se apreto
+            #region
+            //si pongo # y mas abajo #endregio, me permite minizar todo ese codigo entre los #
+            // el rectangulo se hace con alt+219
+
+            for (int i = 0; i < 20; i++)//verticales
+            {
+                Console.CursorLeft = 1;//ubica al cursor en la izquierda al numero que le asigno
+                Console.CursorTop = i;
+                Console.Write("█");
+                Console.CursorLeft = 80;//ubica al cursor en la izquierda al numero que le asigno
+                Console.CursorTop = i;
+                Console.Write("█");
+            }
+            for (int i = 1; i <= 80; i++)//verticales
+            {
+                Console.CursorLeft = i;//ubica al cursor en la izquierda al numero que le asigno
+                Console.CursorTop = 1;
+                Console.Write("█");
+                Console.CursorLeft = i;//ubica al cursor en la izquierda al numero que le asigno
+                Console.CursorTop = 20;
+                Console.Write("█");
+            }
+            #endregion//hacerCancha  //hacerCancha HACER CANCHA
+            do
+            {             
+                while(!Console.KeyAvailable)
+                {
+                    //jugador1.mover();
+                    foreach(Jugador jugador in jugadores)
+                    {
+                        if(jugador!=null)
+                            jugador.mover();
+                    }                    
+                    Thread.Sleep(100);
+                }
+                //Console.WriteLine();
+                consoleKeyInfo = Console.ReadKey();
+                if(consoleKeyInfo.Key == ConsoleKey.N)
+                {
+                    if(Jugador.numeroJugadores < jugadores.Length)
+                    {
+                        jugadores[Jugador.numeroJugadores] = new Jugador("Jugador " + (Jugador.numeroJugadores + 1, false));
+                    }
+                    /*for (int i = 0; i < jugadores.Length; i++)
+                    {                   
+                        if(jugadores[i] is null)
+                        {
+                            jugadores[i] = new Jugador("Jugador " + Jugador.numeroJugadores, false);
+                            break;
+                        }                            
+                    }*/
+                }
+
+                foreach (Jugador jugador in jugadores)
+                {
+                    if(jugador != null)
+                    {
+                        switch (consoleKeyInfo.Key)//con esto hago el movimiento del jugador
+                        {
+                            case ConsoleKey.UpArrow:
+                                jugador.movimiento = ARRIBA;
+                                break;
+
+                            case ConsoleKey.DownArrow:
+                                jugador.movimiento = ABAJO;
+                                break;
+
+                            case ConsoleKey.LeftArrow:
+                                jugador.movimiento = IZQUIERDA;
+                                break;
+
+                            case ConsoleKey.RightArrow:
+                                jugador.movimiento = DERECHA;
+                                break;
+                        }
+                    }
+                    
+                }                
+
+            } while (consoleKeyInfo.KeyChar != 27);
+        }
+    }
+}
