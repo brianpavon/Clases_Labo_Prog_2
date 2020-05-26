ac5d8741c84975f1aa1bf217497a3cf10271b2eb
brianpavon <53911453+brianpavon@users.noreply.github.com>
2020-05-26 20:57:32 -0300

    AGREGO EXPLICACION DE GENERICS

---
+++ Clase_Generic/Generic/Heroe.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public enum ETIpoHeroe
+    {
+        Guerrero,
+        Mago
+
+    }
+    class Heroe<T> //<"Letra"> con esto hago que sea genericy hace que reciba otros parametros
+    {                     //por convencion  se pone la letra en mayuscula y de la A a la Z
+        int vida;
+        int cantMana;
+        ETIpoHeroe tIpoHeroe;
+        //string aux;
+        T aux; //con ese generico hago que lo que le pase, sea un strin
+
+        public Heroe(int vida, int mana,ETIpoHeroe tipo,T aux)
+        {
+            this.vida = vida;
+            this.cantMana = mana;
+            this.tIpoHeroe = tipo;
+            this.aux = aux;
+        }
+
+    }
+}
---
+++ Clase_Generic/Generic/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Heroe<string> miHeroe1 = new Heroe<string>(100,200,ETIpoHeroe.Guerrero,"");
+
+        }
+    }
+}
