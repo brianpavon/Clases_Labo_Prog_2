912b2d379e2069ae6ebd320bff564f1365c3da42
brianpavon <53911453+brianpavon@users.noreply.github.com>
2020-05-26 22:24:51 -0300

    PRACTICA GENERICS

---
+++ Practica_Genericos/Ejercicio_47/Equipo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_47
+{
+    abstract class Equipo
+    {
+        string nombre;
+        DateTime fechaCreacion;
+
+
+    }
+}
