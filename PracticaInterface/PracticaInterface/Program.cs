4edd9d2eb22f2e9ff4762f47e0a7178d51f0d0b1
brianpavon <53911453+brianpavon@users.noreply.github.com>
2020-06-10 10:10:11 -0300

    agrego practica de interfaces

---
+++ PracticaInterface/PracticaInterface/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Prueba prueba = new Prueba();
+
+            //para acceder explicitamente a los metodos de las interfaz, necesitamos castear ((IExpilicita)prueba).
+            //nos da la ventaja de usar 2 metodos que se llamen igual pero que sean de interfaz diferentes
+
+
+            prueba.Implementacion();//explicita, llamo al metodo de la clase
+            ((IExplicita)prueba).Implementacion();// se llama implicitamente
+            ((IExplicita)prueba).Implementacion();// se llama implicitamente
+            
+        }
+    }
+}
---
+++ PracticaInterface/PracticaInterface/Prueba.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaInterface
+{
+    class Prueba :IExplicita
+    {
+        public bool Implementacion()//implicit
+        {
+            return false;
+        }
+
+        bool IExplicita.Implementacion()//explicita, sirve para diferenciar si hay varias clases que la implementen
+        {
+            return true;
+        }
+    }
+}
